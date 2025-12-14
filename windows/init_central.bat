c8acb45252aad2c5a45f63a7ec5e2a20eed6f70d
Alex Ros <rosarlanzon5@gmail.com>
2025-12-15 00:54:33 +0100

    Correccion .bat

--- windows/init_central.bat
+++ windows/init_central.bat
@@ -4,6 +4,6 @@ docker compose up -d --build
 pip install -r requirements.txt
 python topics.py
 cls
-start "BASE DE DATOS" cmd /k "python3 db/server.py"
-start "API CENTRAL" cmd /k "python3 ev_central/api_central.py"
+start "BASE DE DATOS" cmd /k "python db/server.py"
+start "API CENTRAL" cmd /k "python ev_central/api_central.py"
 python ev_central/ev_central.py --broker 127.0.0.1:9092
